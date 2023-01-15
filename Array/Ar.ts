7708a4f45c9105480b1221783c38d79244f3c57d
Debadyuti <debadyuti.karmakar@gmail.com>
2023-01-16 02:26:21 +0530

    reduce

--- Array/Ar.ts
+++ Array/Ar.ts
@@ -3,8 +3,8 @@
 let arr : string[];
 arr = ["java", "c", "c++", "python", "javascript"];
 arr.push("c#");
-let lang = arr.find((lang)=> lang.length>3);
-console.log(lang);
+// let lang = arr.find((lang)=> lang.length>3);
+// console.log(lang);
 let deplist : Array<string>;
 deplist = ["java", "c", "c++", "python", "javascript"];
 
@@ -14,5 +14,8 @@ numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let result = numlist.filter((num)=> num%2==0);
 console.log(result);
 
-let res = numlist.find((num)=> num%1==0);
-console.log(res);
+// let res = numlist.find((num)=> num%1==0);
+// console.log(res);
+
+let sum = numlist.reduce((num1, num2)=> num1+num2);
+console.log(sum);
